50f9148c848329fe85e80ea49fb89724615eb890
Goldy Verma <749947@student.tor.loc>
2019-06-12 17:03:25 -0400

    final commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function Strawberry(){
+const calories = 180;
+const calories2 = '180';
+console.log(calories+1);
+console.log(calories2+1);}
+const ShowForm = false;
+console.log(ShowForm);
---
+++ javascript2.js
@@ -0,0 +1,6 @@
+const meal2={
+    description: 'Breakfast',
+    calories:180,
+};
+console.log("Now i am in my JS file")
+console.log(meal2.description);
